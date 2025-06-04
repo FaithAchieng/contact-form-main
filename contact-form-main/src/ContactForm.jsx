68bb6af5cf86729098503cb016087604b1f1cca2
FaithAchieng <faithachienglexus@gmail.com>
2025-06-04 14:08:27 +0300

    handled errors

--- contact-form-main/src/ContactForm.jsx
+++ contact-form-main/src/ContactForm.jsx
@@ -1,60 +1,131 @@
+function ContactForm() {
+  function handleSubmission(event) {
+    event.preventDefault();
 
-function ContactForm(){
-  
+    const firstname = document.getElementById("fname-input");
+    const firstnameError = document.getElementById("fname-error");
+    const lastname = document.getElementById("lname-input");
+    const lastnameError = document.getElementById("lname-error");
+    const email = document.querySelector(".email-input");
+    const emailError = document.querySelector(".email-error");
+    const queryGeneral = document.getElementById("general");
+    const querySupport = document.getElementById("support");
+    const queryError = document.querySelector(".query-error");
+    const message = document.querySelector(".message-textarea");
+    const messageError = document.querySelector(".message-error");
+    const consent = document.getElementById("consent");
+    const consentError = document.querySelector(".consent-error");
 
- return(
-    <div className='container'>
-        <div className='main-card'>
-                    
-                    <h1>Contact Us</h1> 
-                    <form action=""> 
-            <div className="container-2 ">
-                        
-  <label className="fname">First Name<span className='asterix'>*</span>  </label>
-  <label className="lname">Last Name <span className='asterix'>*</span></label>
+    if (firstname.value.trim() === "") {
+      firstnameError.style.display = "block";
+    } else {
+      firstnameError.style.display = "none";
+    }
 
-  <input type="text" className="fname-input" id="fname-input" />
-  <input type="text" className="lname-input"  />
+    if (lastname.value.trim() === "") {
+      lastnameError.style.display = "block";
+    } else {
+      lastnameError.style.display = "none";
+    }
 
-  <label className="email-label">Email Address  <span className='asterix'>*</span></label>
-  <input type="email" className="email-input"  />
+    if (!email.checkValidity()) {
+      emailError.style.display = "block";
+    } else {
+      emailError.style.display = "none";
+    }
 
-  <label className="query-label">Query Type  <span className='asterix'>*</span></label>
-  <div className="radio1">
-    <input type="radio" id="general" name="query" />
-    <label htmlFor="general">General Enquiry</label>
-  </div>
-  <div className="radio2">
-    <input type="radio" id="support" name="query" />
-    <label htmlFor="support">Support Request</label>
-  </div>
+    if (!queryGeneral.checked && !querySupport.checked) {
+      queryError.style.display = "block";
+    } else {
+      queryError.style.display = "none";
+    }
 
-  <label className="message-label">Message  <span className='asterix'>*</span></label>
-  <input className="message-textarea"></input>
+    if (message.value.trim() === "") {
+      messageError.style.display = "block";
+    } else {
+      messageError.style.display = "none";
+    }
 
-  <div className="checkbox">
-    <input type="checkbox" id="consent" />
-    <label htmlFor="consent">I consent to being contacted by the team  <span className='asterix'>*</span></label>
-  </div>
+    if (!consent.checked) {
+      consentError.style.display = "block";
+    } else {
+      consentError.style.display = "none";
+    }
+  }
 
-  <button className="submit" >Submit</button>
-
-
+  return (
+    <div className="container">
+      <div className="main-card">
+        <h1>Contact Us</h1>
+        <form onSubmit={handleSubmission}>
+          <div className="container-2">
+            <label className="fname">
+              First Name<span className="asterix">*</span>
+            </label>
+            <label className="lname">
+              Last Name <span className="asterix">*</span>
+            </label>
+            <div className="fname-error" id="fname-error">
+              This field is required
             </div>
-                
-                </form> 
+            <div className="lname-error" id="lname-error">
+              This field is required
+            </div>
 
-                {/* Message Sent!
-                Thanks for completing the form. We'll be in touch soon! */}
-                
-                <div className="attribution">
-                    Challenge by <a href="https://www.frontendmentor.io?ref=challenge">Frontend Mentor</a>. 
-                    Coded by <a href="#">Faith Achieng</a>.
-                </div>
+            <input type="text" className="fname-input" id="fname-input" />
+            <input type="text" className="lname-input" id="lname-input" />
 
+            <label className="email-label">
+              Email Address <span className="asterix">*</span>
+            </label>
+            <input type="email" className="email-input" />
+            <div className="email-error">Please enter a valid email address</div>
+
+            <label className="query-label">
+              Query Type <span className="asterix">*</span>
+            </label>
+            <div className="radio1">
+              <input type="radio" id="general" name="query" />
+              <label htmlFor="general">General Enquiry</label>
+            </div>
+            <div className="radio2">
+              <input type="radio" id="support" name="query" />
+              <label htmlFor="support">Support Request</label>
+            </div>
+            <div className="query-error">Please select a query type</div>
+
+            <label className="message-label">
+              Message <span className="asterix">*</span>
+            </label>
+            <input className="message-textarea" />
+            <div className="message-error">This field is required</div>
+
+            <div className="checkbox">
+              <input type="checkbox" id="consent" />
+              <label htmlFor="consent">
+                I consent to being contacted by the team{" "}
+                <span className="asterix">*</span>
+              </label>
+            </div>
+            <div className="consent-error">
+              To submit this form, please consent to being contacted
+            </div>
+
+            <button className="submit" type="submit">
+              Submit
+            </button>
+          </div>
+        </form>
+
+        <div className="attribution">
+          Challenge by{" "}
+          <a href="https://www.frontendmentor.io?ref=challenge">
+            Frontend Mentor
+          </a>
+          . Coded by <a href="#">Faith Achieng</a>.
         </div>
-
+      </div>
     </div>
- )
+  );
 }
-export default ContactForm
+export default ContactForm;
